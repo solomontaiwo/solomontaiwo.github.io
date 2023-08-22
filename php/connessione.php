5f67ed77b9a2c6675c72c3bf3fa21e729c29f876
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-08-22 13:47:34 +0200

    update index removing project references

--- php/connessione.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$link = mysqli_connect("127.0.0.1", "userBibliotecaUNIFE", "user123", "BibliotecaUNIFE");
-
-if (!$link) {
-	echo "Impossibile collegarsi al database. Per cambiare le credenziali modifica il file /php/connessione.php";
-	exit(-1);
-}
